6d98f9e6d441476028876fa5552aa3e5366490d0
Raghu Betina <raghu@firstdraft.com>
2018-04-12 06:58:51 +0000

    Direct association between venues and bookmarks

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,8 @@
 class Bookmark < ApplicationRecord
   # Direct associations
 
+  belongs_to :venue
+
   belongs_to :user
 
   # Indirect associations
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,6 +1,10 @@
 class Venue < ApplicationRecord
   # Direct associations
 
+  has_many   :best_dishes,
+             :class_name => "Bookmark",
+             :dependent => :destroy
+
   has_many   :dishes,
              :dependent => :destroy
 
